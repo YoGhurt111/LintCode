218528efb52a9afae3f910518bc64d5c0dc4d27a
YoGhurt <623770278@qq.com>
2017-09-12 20:42:53 +0800

    LeetCode Valid Parentheses #20

---
+++ ValidParentheses.java
@@ -0,0 +1,21 @@
+// LeetCode Valid Parentheses #20
+public class ValidParentheses{
+  public static void main(String[] args) {
+
+  }
+
+  public static boolean isValid(String s) {
+	   Stack<Character> stack = new Stack<Character>();
+	    for (char c : s.toCharArray()) {
+		      if (c == '(')
+			       stack.push(')');
+		      else if (c == '{')
+			       stack.push('}');
+		      else if (c == '[')
+			       stack.push(']');
+		      else if (stack.isEmpty() || stack.pop() != c)
+			       return false;
+	     }
+	      return stack.isEmpty();
+}
+}
